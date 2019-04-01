f4932698131bf2813fd4e1542533ae4114ae5790
IzzyFitton <isf21@bath.ac.uk>
2019-04-01 14:17:44 +0100

    Tried to fix mail problem
    Played around with the mailgun - put my own api in etc.
    
    It will send the message if you change 'to' to an actual email address, but it won't if you have 'email' suggesting it is not taking the data from the form!
    
    I have put it back to email for now - but still has my mailgun api in.

--- Pythonapp.py
+++ Pythonapp.py
@@ -1,14 +1,12 @@
 import requests
 def send_simple_message(email):
     return requests.post(
-        "https://api.mailgun.net/v3/sandbox6eca69341ec64cbe81055f003ef578db.mailgun.org/messages",
-        auth=("api", "2dbcea6e515b8c91a9271664618d809f-de7062c6-bb2cb17d"),
-        data={"from": "Excited User <mailgun@sandbox6eca69341ec64cbe81055f003ef578db.mailgun.org>",
+        "https://api.mailgun.net/v3/sandbox2d1816a7301044609f33f7ca54f560be.mailgun.org/messages",
+        auth=("api", "3018c97283cb2b10930699e29802a5d5-de7062c6-fe66ae0c"),
+        data={"from": "Quiz Team <mailgun@sandbox2d1816a7301044609f33f7ca54f560be.mailgun.org>",
               "to": email,
               "subject": "Pub Quiz",
               "text": "The correct answers were Kaley Cuoco, Boston, and Ben-Hur! "})
-
-
 
 from flask import Flask, render_template, request #second one is capitalised, module and then library
 app = Flask("MailTestApp")
@@ -39,7 +37,7 @@ def shutdown_server():
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
-    
+
 @app.route('/shutdown', methods=['GET'])
 def shutdown():
     shutdown_server()
